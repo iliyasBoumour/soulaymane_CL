2192a49e1a2e24d4f4dc7a07c43e66c49f0051e9
iliyasBr <iliyasboumour@gmail.com>
2022-12-02 22:52:38 +0100

    fixes

--- src/hooks/useMyOffers.ts
+++ src/hooks/useMyOffers.ts
@@ -1,10 +1,23 @@
 import axios from 'axios';
+import { useContext } from 'react';
 import { useQuery } from 'react-query';
+import { Store } from '../context/Store';
+import { getHeader } from '../utils/AuthorizationConfig';
 
 export const useMyOffers = () => {
+  const {
+    state: {
+      auth: { token },
+    },
+  } = useContext(Store);
   const getMyOffers = async () => {
+    if (!token) {
+      throw new Error('No token');
+    }
+    const config = getHeader(token);
     const { data } = await axios.get(
       `${process.env.REACT_APP_API_URL}/offers/my`,
+      config,
     );
     return data;
   };
